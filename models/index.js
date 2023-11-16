a232eae27abdccf40a1f3a5240f009c20b9d85ed
mintyry <gitmintyry@gmail.com>
2023-11-15 23:49:09 -0800

    set up associations in model/index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+//ASK ABOUT FOREIGN KEYS
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,13 +7,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag',
+  },
+  foreignKey: 'tag_id'
+  //do we need otherKey?
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag',
+  },
+  foreignKey: 'product_id'
+  //do we need otherKey?
+});
 module.exports = {
   Product,
   Category,
