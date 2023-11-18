eac981136bd6c03a898291d03eafea70615deb2c
mintyry <gitmintyry@gmail.com>
2023-11-17 20:29:02 -0800

    added comments to server.js

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//handles json accordingly
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//uses routes in this folder
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
